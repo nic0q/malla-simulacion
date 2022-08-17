0ec61b7aa96da5c452cbceac914edaca3c533f45
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-08-16 23:10:38 -0400

    Updates

---
+++ precache-manifest.4bd89a97a1e11a6b8e4f5781705522fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "11876dbe272abc2abaa6",
+    "url": "/static/css/main.62d1b4c8.chunk.css"
+  },
+  {
+    "revision": "11876dbe272abc2abaa6",
+    "url": "/static/js/main.11876dbe.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "8e9ce94223c73231b58d",
+    "url": "/static/js/2.8e9ce942.chunk.js"
+  },
+  {
+    "revision": "a850c70773011bd5f111",
+    "url": "/static/js/runtime~main.a850c707.js"
+  },
+  {
+    "revision": "000a9e498b32b90950b17f4a8274426d",
+    "url": "/index.html"
+  }
+];
