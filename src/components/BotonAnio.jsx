bfbb6d201348b84dc978c07e1e98218807ff6836
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-08-18 22:51:11 -0400

    refactor: font

--- src/components/BotonAnio.jsx
+++ src/components/BotonAnio.jsx
@@ -1,5 +1,5 @@
 export default function BotonAnio({año, handleClick}){
   return <div onClick={()=>handleClick(año)}>
-    <p className="w-72 fixed bg-gray-200 hover:bg-gray-400">Año {año}</p>  
+    <p className={`fixed bg-gray-200 hover:bg-gray-400 ${año === "5/2" ? "w-36" : "w-72 "}`}>Año {año}</p>  
     </div>
 }
